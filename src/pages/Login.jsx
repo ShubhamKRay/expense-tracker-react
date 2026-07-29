a6a2a2cc24603ada56df4a6d549c59526482343f
ShubhamKRay <shubhairmen@gmail.com>
2026-07-30 03:00:21 +0530

    Implement login form using React useState

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,42 @@
+import { useState } from "react";
 import "../css/Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  function handleLogin() {
+    console.log(email);
+
+    console.log(password);
+  }
+
   return (
     <div className="login-container">
       <h1>My Expense Tracker</h1>
       <p>Login to your account</p>
 
-      <input type="email" placeholder="Enter Email" />
+      <input
+        type="email"
+        placeholder="Enter Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
 
       <br />
       <br />
 
-      <input type="password" placeholder="Enter Password" />
+      <input
+        type="password"
+        placeholder="Enter Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <br />
       <br />
 
-      <button>Sign In</button>
+      <button onClick={handleLogin}>Sign In</button>
     </div>
   );
 }
